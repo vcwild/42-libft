b067d192de4e63b622d8140b334162bc4a63bd47
vcwild <vcwildm@gmail.com>
2021-05-20 23:28:31 -0300

    Deleted test files

--- src/test/ft_tolower.c
+++
@@ -1,9 +0,0 @@
-unsigned char	ft_tolower(char c)
-{
-	unsigned int	dist;
-
-	dist = 'a' - 'A';
-	if (c >= 'A' && c <= 'Z')
-		c = c + dist;
-	return ((unsigned char)c);
-}
--- src/test/ft_toupper.c
+++
@@ -1,9 +0,0 @@
-unsigned char	ft_toupper(char c)
-{
-	unsigned int	dist;
-
-	dist = 'a' - 'A';
-	if (c >= 'a' && c <= 'z')
-		c = c - dist;
-	return ((unsigned char)c);
-}
