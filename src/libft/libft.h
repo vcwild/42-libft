52aad89340f26d8ec87a96c2de62ac35e2d0a4c0
Victor <vcwildm@gmail.com>
2021-05-25 19:17:36 -0300

    update header with bonus files

--- src/libft/libft.h
+++ src/libft/libft.h
@@ -6,7 +6,7 @@
 # include <unistd.h>
 # include <limits.h>
 
-typedef struct s_list
+typedef struct	s_list
 {
 	void			*content;
 	struct s_list	*next;
@@ -45,4 +45,12 @@ void			ft_putstr_fd(char const *s, int fd);
 void			ft_putendl_fd(char *s, int fd);
 void			ft_putnbr_fd(int n, int fd);
 
+t_list			*ft_lstnew(void *content);
+void			ft_lstadd_front(t_list **lst, t_list *new);
+int				ft_lstsize(t_list *lst);
+t_list			*ft_lstlast(t_list *lst);
+void			ft_lstadd_back(t_list **lst, t_list *new);
+void			ft_lstdelone(t_list *lst, void (*del)(void*));
+void			ft_lstclear(t_list **lst, void (*del)(void*));
+void			ft_lstiter(t_list *lst, void (*f)(void *));
 #endif
