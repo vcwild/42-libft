362e4026285eb4e0c197f321983117fab521cc5b
Victor <vcwildm@gmail.com>
2021-05-25 15:51:03 -0300

    create list add to front

---
+++ src/libft/ft_lstadd_front.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+/**
+ * @brief Adds the element ’new’ at the beginning of the list.
+ * 
+ * @param lst The address of a pointer to the first link of a list
+ * @param new The address of a pointer to the element to be added to the list
+ */
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	t_list	*tmp;
+
+	tmp = *lst;
+	*lst = new;
+	new->next = tmp;
+}
