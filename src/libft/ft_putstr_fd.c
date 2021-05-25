be8c433fef2a8971abdfcc924d37d1ae76172eb2
Victor <vcwildm@gmail.com>
2021-05-25 14:16:09 -0300

    create ft_putstr_fd

---
+++ src/libft/ft_putstr_fd.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+/**
+ * @brief Outputs the string ’s’ to the given filedescriptor.
+ *
+ * @param s The string to output
+ * @param fd The file descriptor on which to write
+ */
+void	ft_putstr_fd(char const *s, int fd)
+{
+	if (s)
+	{
+		while(*s)
+			write(fd, s++, 1);
+	}
+}
