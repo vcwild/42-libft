91c7d03f0006de75e3d568a14db63754d187cd9c
Victor <vcwildm@gmail.com>
2021-05-25 14:59:35 -0300

    create ft_putnbr_fd

---
+++ src/libft/ft_putnbr_fd.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+
+/**
+ * @brief Outputs the integer ’n’ to the given filedescriptor.
+ *
+ * @param n The integer to output
+ * @param fd The file descriptor on which to write
+ */
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		if (n == INT_MIN)
+		{
+			ft_putchar_fd('2', fd);
+			ft_putnbr_fd(147483648, fd);
+			return ;
+		}
+		n = -n;
+	}
+	if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+	}
+	ft_putchar_fd(n % 10 + 48, fd);
+}
