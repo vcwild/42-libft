1d05d75d8bbe4998a5f5d34e8f41228cd0bb7e1d
Victor <vcwildm@gmail.com>
2021-05-26 11:30:43 -0300

    create list iteration function

--- src/libft/ft_lstiter.c
+++ src/libft/ft_lstiter.c
@@ -3,11 +3,16 @@
 /**
  * @brief Iterates the list ’lst’ and applies the function
  * ’f’ to the content of each element.
- * 
+ *
  * @param lst The adress of a pointer to an element
- * @param f The adress of the function used to iterate onthe list
+ * @param f The adress of the function used to iterate on the list
  */
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	
+	if (lst && f)
+	{
+		f(lst);
+		if (lst->next)
+			ft_lstiter(lst->next, f);
+	}
 }
