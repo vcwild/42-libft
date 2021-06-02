8b8e444475327495214cf66f609e6afb73102a3e
Victor <vcwildm@gmail.com>
2021-06-02 19:07:21 -0300

    simplify strjoin

--- src/libft/ft_strjoin.c
+++ src/libft/ft_strjoin.c
@@ -6,10 +6,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*str;
 	char	*pos;
 
+	if (!s1 || !s2)
+		return (NULL);
 	str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
-	if (str == NULL
-		|| !s1
-		|| !s2)
+	if (str == NULL)
 		return (NULL);
 	pos = str;
 	while (*s1)
