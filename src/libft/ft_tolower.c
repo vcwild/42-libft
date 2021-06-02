c689d8f61b9478c0db7f223e14fdffbed0161b48
Victor <vcwildm@gmail.com>
2021-06-02 19:08:01 -0300

    simplify tolower increment rule

--- src/libft/ft_tolower.c
+++ src/libft/ft_tolower.c
@@ -1,11 +1,6 @@
 int	ft_tolower(int c)
 {
-	char	dist;
-	char	str;
-
-	dist = 'a' - 'A';
-	str = (char)c;
-	if (str >= 'A' && str <= 'Z')
-		str += dist;
-	return ((int)str);
+	if (c >= 'A' && c <= 'Z')
+		c += 32;
+	return (c);
 }
